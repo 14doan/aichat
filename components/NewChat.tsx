722415e14edbf6d388d704828fef829fd25fb01f
14doan <14doan@gmail.com>
2023-09-07 11:14:59 +0100

    addcollection

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -10,17 +10,35 @@ export default function NewChat() {
   const { data: session } = useSession();
   const router = useRouter();
 
-  const createNewChat = async () => {
-    const doc = await addDoc(
-      collection(db, 'users', session?.user?.email!, 'chats'),
-      {
-        userId: session?.user?.email!,
-        createdAt: serverTimestamp(),
-      }
-    );
+  // const createNewChat = async () => {
+  //   const doc = await addDoc(
+  //     collection(db, 'users', session?.user?.email!, 'chats'),
+  //     {
+  //       userId: session?.user?.email!,
+  //       createdAt: serverTimestamp(),
+  //     }
+  //   );
 
-    router.push(`/chat/${doc.id}`);
-  };
+  //   router.push(`/chat/${doc.id}`);
+  // };
+
+  try {
+    // Get a reference to the user's chats subcollection
+    const userChatsRef = collection(db, 'users', session?.user?.email!, 'chats');
+
+    // Create a new chat document inside the user's chats subcollection
+    const chatDocRef = await addDoc(userChatsRef, {
+      // Add chat data here
+      createdAt: serverTimestamp(),
+      participants: [session.user.email], // Include the current user as a participant
+    });
+
+    // Redirect to the newly created chat using its document ID
+    router.push(`/chat/${chatDocRef.id}`);
+  } catch (error) {
+    console.error('Error creating new chat:', error);
+  }
+};
 
   return (
     <div onClick={createNewChat} className="border border-gray-200 chatRow">
