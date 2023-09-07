f2be500cc4e65629a12c375a4241b1b9cb2c01b3
14doan <14doan@gmail.com>
2023-09-07 12:55:55 +0100

    newchat

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -10,33 +10,16 @@ export default function NewChat() {
   const { data: session } = useSession();
   const router = useRouter();
 
-  // const createNewChat = async () => {
-  //   const doc = await addDoc(
-  //     collection(db, 'users', session?.user?.email!, 'chats'),
-  //     {
-  //       userId: session?.user?.email!,
-  //       createdAt: serverTimestamp(),
-  //     }
-  //   );
-
-  //   router.push(`/chat/${doc.id}`);
-  // };
-
   const createNewChat = async () => {
-    try {
-      const chatRef = collection(db, 'users', session?.user?.email!, 'chats');
-
-      // Create a new chat document with server timestamp
-      const docRef = await addDoc(chatRef, {
+    const doc = await addDoc(
+      collection(db, 'users', session?.user?.email!, 'chats'),
+      {
         userId: session?.user?.email!,
         createdAt: serverTimestamp(),
-      });
+      }
+    );
 
-      // Redirect to the chat page with the newly created document's ID
-      router.push(`/chat/${docRef.id}`);
-    } catch (error) {
-      console.error('Error creating new chat:', error);
-    }
+    router.push(`/chat/${doc.id}`);
   };
 
   return (
